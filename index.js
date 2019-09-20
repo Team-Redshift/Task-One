d1c0afa420b916a14e1e20fc1fb527f92656f3cd
gfredtech <gfredtech@gmail.com>
2019-09-20 12:19:03 +0000

    Restructured, again

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const { login, signup } = require("./controllers/userController");
+const { login, signup, fetchUser } = require("./controllers/userController");
 const port = 3000;
 
 const root = "/api/v1";
 const apiCalls = {
   login: `${root}/login`,
-  signup: `${root}/signup`
+  signup: `${root}/signup`,
+  fetchUser: `${root}/fetchUser`
 };
 
 const app = express();
@@ -15,8 +16,10 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static('public'));
 app.post(apiCalls.login, login);
 app.post(apiCalls.signup, signup);
+app.post(apiCalls.fetchUser, fetchUser);
 
 app.listen(port, () => console.log(`Web service started on port ${port}...`));
 module.exports = { app, apiCalls };
