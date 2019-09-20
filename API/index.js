84c072f370f522ad106877d97b98e44d7960e963
gfredtech <gfredtech@gmail.com>
2019-09-20 11:59:11 +0000

    Done with MVP

--- API/index.js
+++ API/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const { login, signup } = require('./controllers/userController');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { login, signup } = require("./controllers/userController");
 const port = 3000;
 
-const root = '/api/v1';
+const root = "/api/v1";
 const apiCalls = {
-    login: `${root}/login`,
-    signup: `${root}/signup`
-}
+  login: `${root}/login`,
+  signup: `${root}/signup`
+};
 
 const app = express();
 app.use(cors());
@@ -17,10 +17,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post(apiCalls.login, login);
 app.post(apiCalls.signup, signup);
-app.post('/test/', (req, res) => {
-    console.log("called");
-    res.send({status: 'OK'});
-});
 
 app.listen(port, () => console.log(`Web service started on port ${port}...`));
 module.exports = { app, apiCalls };
--- UI/js/main.js
+++ UI/js/main.js
@@ -1,3 +1,5 @@
+const api = "http://127.0.0.1:3000/api/v1";
+
 const checkLogin = results => {
   const incorrect = document.querySelector("p.login-info");
   if (results.status == 200) {
@@ -9,11 +11,24 @@ const checkLogin = results => {
     incorrect.classList.remove("correct");
   }
 };
+
+const doSignup = results => {
+  const message = document.querySelector("p.signup-message");
+  if(results.status == 200) {
+    message.value = "";
+    window.setTimeout(function() {
+      window.location = "./welcome.html";
+    }, 2000);
+  } else {
+    message.value = results.message;
+  }
+}
+
 const loginUser = async () => {
   const username = document.getElementById("email-input").value;
   const password = document.getElementById("password-input").value;
-  const api = "http://127.0.0.1:3000/api/v1/login";
-  await fetch(api, {
+  const loginEndpoint = `${api}/login`;
+  await fetch(loginEndpoint, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
@@ -23,4 +38,30 @@ const loginUser = async () => {
     .then(res => res.json())
     .then(data => checkLogin(data))
     .catch(err => console.log(err));
+};
+
+const signupUser = async () => {
+  const incorrect = document.querySelector("p.login-info");
+
+  const fullname = document.getElementById("name-input").value;
+  const username = document.getElementById("username-input").value;
+  const password = document.getElementById("password-input").value;
+  const password2 = document.getElementById("confirm-password-input").value;
+  console.log(fullname, username, password, password2);
+
+  if (password !== password2) {
+    incorrect.classList.remove("correct");
+  } else {
+  const signupEndpoint = `${api}/signup`;
+    await fetch(signupEndpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ name: fullname, username: username, password: password })
+    })
+      .then(res => res.json())
+      .then(data => doSignup(data))
+      .catch(err => console.log(err));
+  }
 };
