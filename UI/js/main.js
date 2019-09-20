369c4674d0e1383306d61a94822fd3e1da9ce0c7
gfredtech <gfredtech@gmail.com>
2019-09-20 10:46:48 +0000

    tiny fix

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,25 +1,26 @@
-const checkLogin = (results) => {
-    if (results.status == 200) {
-        incorrect.classList.add('correct');
-        window.setTimeout(function () {
-            window.location = "./welcome.html";
-        }, 2000);
-    } else {
-        const incorrect = document.querySelector('p.login-info');
-        incorrect.classList.remove('correct');
-    }
-}
+const checkLogin = results => {
+  const incorrect = document.querySelector("p.login-info");
+  if (results.status == 200) {
+    incorrect.classList.add("correct");
+    window.setTimeout(function() {
+      window.location = "./welcome.html";
+    }, 2000);
+  } else {
+    incorrect.classList.remove("correct");
+  }
+};
 const loginUser = async () => {
-    const username = document.getElementById('email-input').value;
-    const password = document.getElementById('password-input').value;
-    const api = "http://127.0.0.1:3000/api/v1/login";
-    await fetch(api, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username: username, password: password }),
-    }).then(res => res.json())
-        .then(data => checkLogin(data))
-        .catch((err) => console.log(err));
+  const username = document.getElementById("email-input").value;
+  const password = document.getElementById("password-input").value;
+  const api = "http://127.0.0.1:3000/api/v1/login";
+  await fetch(api, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ username: username, password: password })
+  })
+    .then(res => res.json())
+    .then(data => checkLogin(data))
+    .catch(err => console.log(err));
 };
