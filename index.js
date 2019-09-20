0bf46587de3e88a003a05681e59bc7b9a953564e
gfredtech <gfredtech@gmail.com>
2019-09-20 12:29:21 +0000

    fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { login, signup, fetchUser } = require("./controllers/userController");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const root = "/api/v1";
 const apiCalls = {
